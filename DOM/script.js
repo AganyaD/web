b9b1752e6ba71f17d27f3d00d7eb3c4f73d78fe9
aganya dejen <aganya.dejen@gmail.com>
2019-02-14 02:01:47 +0200

    date countdwon

--- DOM/script.js
+++ DOM/script.js
@@ -1,25 +1,42 @@
 var h1 =document.querySelector("h1");
+var h2 =document.querySelector("h2");
 var btn = document.querySelector("button");
 btn.addEventListener("click",clickE);
 var flg = false;
 setInterval(changeColor,500);
 
+var weddingday = {
+
+}
 function changeColor()
 {
     if (flg)
     {
-        console.log("start function")
-        if(h1.style.color == "red")
-        {
-            console.log("change color to blue")
-            h1.style.color = "blue";
-            h1.textContent ="change color to blue";
-        }
-        else
-        {
-            h1.style.color = "red";
-            h1.textContent = "color is red";
-        }
+        var currentdate = new Date();
+        var weddingday = new Date(2019,7,27);
+        var datetime = "Last Sync: " + currentdate.getDay() + "/"+currentdate.getMonth() 
+                        + "/" + currentdate.getFullYear() + " @ " 
+                        + currentdate.getHours() + ":" 
+                        + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+        h1.textContent = datetime;
+        // var kjkj = new date(Number(weddingday.getFullYear()) - currentdate.getFullYear(),
+        // Number(weddingday.getMonth()) - Number(currentdate.getMonth()),
+        // Number(weddingday.getDay()) - Number(currentdate.getDay()),
+        // Number( weddingday.getHours()) - Number(currentdate.getHours()),
+        // Number( weddingday.getMinutes()) -Number( currentdate.getMinutes()),
+        // Number(weddingday.getSeconds()) -Number( currentdate.getSeconds()));
+        // console.log(weddingday.get());
+        console.log(weddingday.getMonth());
+        // weddingday -= currentdate;
+        var diff = new Date(weddingday.getTime() - currentdate.getTime());
+        var datetime = "Last Sync: " + diff.getDay() + "/"+diff.getMonth() 
+                        + "/" + (diff.getFullYear() - 1970) + " @ " 
+                        + diff.getHours() + ":" 
+                        + diff.getMinutes() + ":" + diff.getSeconds();
+
+        h2.textContent = datetime;
+        // (currentdate - new Date(2019,7,27,19,30,00);
+        
 
     }   
 
